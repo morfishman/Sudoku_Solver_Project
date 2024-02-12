95f086a75b6b777bb8b9ef76bd90b1707baff944
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-12 13:29:32 +0200

    Added IFileInputProvider interface and FileInputProviders directory
    This commit introduces the IFileInputProvider interface in the FileInputProviders directory. The interface extends the IInputProvider interface and includes a FilePath property, enabling file path management for file input providers.

---
+++ SudokuSolver/IOUtilities/FileInputProviders/IFileInputProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.IOUtilities.FileInputProviders
+{
+    interface IFileInputProvider<fileType> : IInputProvider<fileType>
+    {
+        string FilePath { set; }
+    }
+} 
