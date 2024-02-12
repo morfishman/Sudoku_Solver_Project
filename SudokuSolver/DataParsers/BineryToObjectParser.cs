e2ec2aa5ee9261686dd133c254cc13ae059fbcbd
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-12 23:03:32 +0200

    Implemented a binary-to-object parser in the SudokuSolver.DataParsers namespace.
    This commit introduces the BineryToObjectParser class, which implements the IDataParser interface to parse byte arrays into objects of a specified type. The ParseData method deserializes the byte array using a BinaryFormatter and ensures that the deserialized object is of the expected type before returning it. Additionally, appropriate documentation and exception handling are provided to ensure clarity and reliability in the parsing process.

---
+++ SudokuSolver/DataParsers/BineryToObjectParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace SudokuSolver.DataParsers
+{
+    public class BineryToObjectParser<Treturnobj> : IDataParser<byte[], Treturnobj>
+    {
+        private byte[] dataToParse;
+
+        BineryToObjectParser(byte[] dataToParse)
+        {
+            this.dataToParse = dataToParse;
+        }
+
+
+        /// <summary>
+        /// Deserializes a byte array into an object of type Treturnobj.
+        /// </summary>
+        /// <param name="dataToParse">The byte array to deserialize.</param>
+        /// <typeparam name="Treturnobj">The type of object to deserialize into.</typeparam>
+        /// <returns>The deserialized object of type Treturnobj.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if dataToParse is null.</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if there is an issue during deserialization.</exception>
+        /// <exception cref="System.InvalidCastException">Thrown if the deserialized object is not of the expected type Treturnobj.</exception>
+        public Treturnobj ParseData()
+        {
+            if (dataToParse == null)
+                throw new ArgumentNullException(nameof(dataToParse));
+
+            using (MemoryStream memoryStream = new MemoryStream(dataToParse))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                object deserializedObject = formatter.Deserialize(memoryStream);
+
+                // Check if the deserialized object is of the expected type
+                if (deserializedObject is Treturnobj)
+                {
+                    return (Treturnobj)deserializedObject;
+                }
+                else
+                {
+                    throw new InvalidCastException($"Deserialized object is not of type {typeof(Treturnobj)}");
+                }
+            }
+
+        }
+    }
+}
