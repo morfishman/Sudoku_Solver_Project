1819f3859175ded8173553bfe9e2d907c42acc4e
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-12 11:30:52 +0200

    Add IOUtilities namespace containing IInputProvider interface
    Added a new namespace 'SudokuSolver.IOUtilities' containing an interface 'IInputProvider' which defines a method 'ReadInput' for retrieving input data of generic type. This interface will facilitate input handling within the SudokuSolver project.

---
+++ SudokuSolver/IOUtilities/IInputProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.IOUtilities
+{
+    interface IInputProvider<inputType>
+    {
+        inputType ReadInput();
+    }
+}
