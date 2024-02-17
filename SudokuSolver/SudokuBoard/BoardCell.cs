71bb5ac5af74e17c366056168248e8461218fc1d
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-18 01:43:29 +0200

    Introduced BoardCell interface.
    This interface serves as a blueprint for concrete cell implementations, ensuring consistency and flexibility across the SudokuSolver project.

---
+++ SudokuSolver/SudokuBoard/BoardCell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.SudokuBoard
+{
+    public interface BoardCell
+    {
+        bool Is_Option_Exists(int Option);
+        void Remove_Option(int Option);
+        void Retrive_Option(int Option);
+        void Set_Current_Value(int Value);
+        bool Is_Permint();
+    }
+}
