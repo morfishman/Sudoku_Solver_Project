029d2cc20007ee17b5a2ba6920a20c52c2d23434
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-18 14:34:09 +0200

    Add interfaces for Sudoku solving
    Added interfaces `ISudokuSolver` and `IStandardSudokuSolver` to define the contract for Sudoku solvers.
    `ISudokuSolver` defines common methods for solving Sudokus, while `IStandardSudokuSolver` extends it to provide specific methods for solving standard Sudokus.
    
    Create new branch "sudoku-solver-interfaces"

---
+++ SudokuSolver/SudokuSolver/ISudokuSolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.SudokuSolver
+{
+    public interface ISudokuSolver
+    {
+        void Solve();
+        string ToString();
+    }
+}
