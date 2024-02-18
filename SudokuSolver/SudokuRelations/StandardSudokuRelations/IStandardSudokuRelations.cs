51c9b00606915e333a09a6bf1f29fbe92520a31a
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-18 14:19:51 +0200

    Implemented IStandardSudokuRelations interface extending ISudokuRelations. Implemented StandardSudokuRelationsRunTime class
    providing runtime calculations for Sudoku relations. Added XML documentation comments to interfaces and class members for improved code documentation and readability.

---
+++ SudokuSolver/SudokuRelations/StandardSudokuRelations/IStandardSudokuRelations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.SudokuRelations.StandardSudokuRelations
+{
+    public interface IStandardSudokuRelations: ISudokuRelations
+    {
+        HashSet<(int, int)> GetRowRelations(int row, int col);
+        HashSet<(int, int)> GetColumnRelations(int row, int col);
+        HashSet<(int, int)> GetBoxRelations(int row, int col);
+    }
+}
