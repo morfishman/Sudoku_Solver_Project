fd8f687d3de1eec71436976f672ecf955ad75778
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-19 10:36:33 +0200

    Pulled from the SudokuBoard Branch

---
+++ SudokuSolver/SudokuBoard/BoardCell/IBoardCell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.SudokuBoard.BoardCell
+{
+    public interface IBoardCell
+    {
+        const int BOARD_CELL_EMPTY = 0; 
+        bool IsOptionExists(int Option);
+        bool RemoveOption(int Option);
+        bool RetriveOption(int Option);
+        void SetCurrentValue(int Value);
+        bool IsPermint();
+        int CountOptions();
+        int GetCurrentValue();
+        byte[] GetOptions();
+        void RemoveOptions(byte[] Options);
+        void SetOptions(byte[] Options);
+    }
+}
