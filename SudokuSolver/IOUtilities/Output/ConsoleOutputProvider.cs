109a4432d95a10020c600132daaa0d4f9b4cdd78
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-19 10:30:26 +0200

    save Pull request from IOUtilities branch

---
+++ SudokuSolver/IOUtilities/Output/ConsoleOutputProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.IOUtilities.Output
+{
+    public class ConsoleOutputProvider:IOutputProvider
+    {
+        private String dataToWrite;
+
+
+        /// <summary>
+        /// Initializes a new instance of the ConsoleOutputProvider class with the specified data to write.
+        /// </summary>
+        /// <param name="dataToWrite">The data to write to the console.</param>
+        public ConsoleOutputProvider(string dataToWrite)
+        {
+            this.dataToWrite = dataToWrite;
+        }
+
+        /// <summary>
+        /// Write the data to write to the console.
+        /// </summary>
+        public void WriteOutput()
+        {
+            Console.Write(dataToWrite);
+        }
+    }
+}
