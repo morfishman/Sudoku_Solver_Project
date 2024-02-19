062e16f5815a4f8048a1a11e49b156d83c896368
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-19 07:15:14 +0200

    Created new Folder for more Orgenized Code

---
+++ SudokuSolver/IOUtilities/Input/IInputProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.IOUtilities.Input
+{
+    public interface IInputProvider<inputType>
+    {
+        inputType ReadInput();
+    }
+}
