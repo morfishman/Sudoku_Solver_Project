098caf31b7df07daf0954f50cdb3adc065e82f22
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-19 13:37:16 +0200

    added StandardSudokuRelationsTests class
    tests the connections between the cells

---
+++ SudokuSolver/SudokuRelations/StandardSudokuRelations/IStandardSudokuRelations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.SudokuRelations.StandardSudokuRelations
+{
+    public interface IStandardSudokuRelations: ISudokuRelations
+    {
+        HashSet<(int, int)> GetRowRelations(int row, int col);
+        HashSet<(int, int)> GetColumnRelations(int row, int col);
+        HashSet<(int, int)> GetBoxRelations(int row, int col);
+        HashSet<(int, int)> GetOnlyCollomRelations(int col);
+        HashSet<(int, int)> GetOnlyRowRelations(int row);
+        HashSet<(int, int)> GetOnlyBoxRelations(int boxIndex);
+
+
+    }
+}
