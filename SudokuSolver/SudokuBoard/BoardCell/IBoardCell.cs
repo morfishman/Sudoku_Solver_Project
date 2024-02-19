bdefda34351f71a0410b8444377fa96be74b5c76
morfishman <2023mor.p@ebin.ort.org.il>
2024-02-19 07:55:36 +0200

    fixed syntax

--- SudokuSolver/SudokuBoard/BoardCell/IBoardCell.cs
+++ SudokuSolver/SudokuBoard/BoardCell/IBoardCell.cs
@@ -7,15 +7,15 @@ namespace SudokuSolver.SudokuBoard.BoardCell
     public interface IBoardCell
     {
         const int BOARD_CELL_EMPTY = 0; 
-        bool Is_Option_Exists(int Option);
-        bool Remove_Option(int Option);
-        bool Retrive_Option(int Option);
-        void Set_Current_Value(int Value);
-        bool Is_Permint();
-        int Count_Options();
-        int Get_Current_Value();
-        byte[] Get_Options();
-        void Remove_Options(byte[] Options);
-        void Set_Options(byte[] Options);
+        bool IsOptionExists(int Option);
+        bool RemoveOption(int Option);
+        bool RetriveOption(int Option);
+        void SetCurrentValue(int Value);
+        bool IsPermint();
+        int CountOptions();
+        int GetCurrentValue();
+        byte[] GetOptions();
+        void RemoveOptions(byte[] Options);
+        void SetOptions(byte[] Options);
     }
 }
